46e2232026f1b936eab6b646175914c02851bd96
Mert Musabeseoglu <109963581+NAMERTEIZM@users.noreply.github.com>
2022-07-28 17:19:36 +0300

    Add files via upload

---
+++ page.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="tr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <title>tusiad2</title>
+</head>
+<body>
+  <div class="container" id="login-container">
+    <img id="beyaz" src="img/beyaz.png">
+ 
+   <div class="main">
+   <div class="row justify-content-center">
+ 
+ 
+ <div id="lrow" class="col-xl-4 ">
+<img id="logo" src="img/logo.png"> 
+
+</div>
+
+ </div>
+ <div id="logoalti" class="row justify-content-center ">
+   <div class="col-xl-4 ">
+  <p></p><p>Web sayfamız yapım aşamasındadır.</p><p></p>
+  <p><span id="span">Çok yakında </span>  hizmetinizde olacaktır.</p>
+
+ 
+
+</div>
+
+ </div>
+ <div  class="row justify-content-center ">
+   <div id="aciklama" class="col-xl-4 ">
+<span>Ücretsiz ön kayıt oluşturun ve siz de TÜSİAD Dönüşümü Başlat
+projesi ile dönüşümü başlatanlar arasında yerinizi alın.</span>
+
+ 
+
+</div>
+
+ </div>
+ <div class="buton text-center">
+ <btn class="btn btn-warning mx-auto" id="butonn">Ön Kayıt</btn>
+   </div>
+
+   
+
+  </div>
+    
+</body>
+</html>
